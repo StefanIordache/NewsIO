2aec725bade1734c7d66cff01d9fd25de8a4099d
AlinaMaican <alinutzamaican@yahoo.com>
2018-11-19 22:16:23 +0200

    modify nav-menu

--- NewsIO/NewsIO.Client/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ NewsIO/NewsIO.Client/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
-  isExpanded = false;
-
-  collapse() {
-    this.isExpanded = false;
-  }
-
-  toggle() {
-    this.isExpanded = !this.isExpanded;
-  }
+export class NavMenuComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {}
 }
