348c279d791ee3012c70427e71ccec0d4b689e0b
Stefan Iordache <iordachestefan1@gmail.com>
2018-12-01 14:29:24 +0200

    ++ Role view model

---
+++ NewsIO/NewsIO.Api/ViewModels/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsIO.Api.ViewModels
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
