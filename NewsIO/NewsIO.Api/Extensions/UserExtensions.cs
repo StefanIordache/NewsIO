515bb5eb2c5228a978ee87cd503f19b9c58caf27
Stefan Iordache <iordachestefan1@gmail.com>
2018-12-28 12:44:56 +0200

    ** Refactoring

--- NewsIO/NewsIO.Api/Extensions/UserExtensions.cs
+++ NewsIO/NewsIO.Api/Extensions/UserExtensions.cs
@@ -18,31 +18,5 @@ namespace NewsIO.Api.Extensions
             var user = await userManager.FindByEmailAsync(email);
             return await signInManager.PasswordSignInAsync(user.UserName, password, isPersistent, shouldLockout);
         }
-
-        public static async Task<IApplicationBuilder> EnsureRolesCreatedAsync(this IApplicationBuilder app, IConfiguration configuration)
-        {
-            try
-            {
-                IServiceScopeFactory scopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
-
-                using (IServiceScope scope = scopeFactory.CreateScope())
-                {
-                    RoleManager<IdentityRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-                    var roles = configuration.GetSection("Roles").AsEnumerable();
-                    foreach (var role in roles)
-                        if (role.Value != null && !roleManager.RoleExistsAsync(role.Value).Result)
-                        {
-                            var newRole = new IdentityRole(role.Value);
-                            await roleManager.CreateAsync(newRole);
-                        }
-                }
-                return app;
-            }
-            catch (Exception e)
-            {
-                return app;
-            }
-        }
     }
 }
