8b119e7b807b44df18183e7a084c9af9b3141a84
Stefan Iordache <iordachestefan1@gmail.com>
2018-12-28 14:44:03 +0200

    ++ NewsRequests and Categories controllers updates (edit method finished)

--- NewsIO/NewsIO.Services/Implementations/NewsRequestService.cs
+++ NewsIO/NewsIO.Services/Implementations/NewsRequestService.cs
@@ -3,6 +3,7 @@ using NewsIO.Data.Models.Application;
 using NewsIO.Services.Intefaces;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,46 @@ namespace NewsIO.Services.Implementations
         public NewsRequestService(ApplicationContext context)
             : base(context)
         { }
+
+        public override async Task UpdateAsync<NewsRequest>(int id, NewsRequest entry)
+        {
+            try
+            {
+                var entryToUpdate = await GetByIdAsync<NewsRequest>(id);
+
+                if (entryToUpdate == null)
+                {
+                    return;
+                }
+
+                foreach (var property in entryToUpdate
+                                    .GetType()
+                                    .GetProperties((BindingFlags.DeclaredOnly
+                                                    | BindingFlags.Public
+                                                    | BindingFlags.Instance)))
+                {
+                    var initialValue = property.GetValue(entryToUpdate);
+                    var newValue = property.GetValue(entry);
+
+                    if (!property.Name.Equals("RequestDate") &&
+                        !property.Name.Equals("RequestedById") &&
+                        !property.Name.Equals("RequestedBy") &&
+                        !Equals(initialValue, newValue))
+                    {
+                        property.SetValue(entryToUpdate, newValue);
+                    }
+                }
+
+
+
+                Context.Set<NewsRequest>().Update(entryToUpdate);
+                await Context.SaveChangesAsync();
+            }
+            catch
+            {
+                return;
+            }
+        }
 
         public async Task<bool> CloseNewsRequestAsync(NewsRequest newsRequest)
         {
