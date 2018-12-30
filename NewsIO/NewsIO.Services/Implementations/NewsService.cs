c71dc7916a611179108121847606f1eb363774c4
Stefan Iordache <iordachestefan1@gmail.com>
2018-12-30 22:45:38 +0200

    ++ NewsService updates (sorting and pagination all over the place)

--- NewsIO/NewsIO.Services/Implementations/NewsService.cs
+++ NewsIO/NewsIO.Services/Implementations/NewsService.cs
@@ -1,8 +1,12 @@
-﻿using NewsIO.Data.Contexts;
+﻿using Microsoft.EntityFrameworkCore;
+using NewsIO.Data.Contexts;
+using NewsIO.Data.Models.Application;
 using NewsIO.Services.Intefaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NewsIO.Services.Implementations
 {
@@ -11,6 +15,248 @@ namespace NewsIO.Services.Implementations
         public NewsService(ApplicationContext context)
             : base(context)
         {
+        }
+
+        public async Task<IEnumerable<News>> GetAlpabeticalByCategoryAsync(Category category)
+        {
+            return await Context
+                .Set<News>()
+                .Where(n => n.CategoryId == category.Id)
+                .OrderBy(n => n.Title)
+                .ToListAsync();
+        }
+
+        public async Task<(IEnumerable<News>, int)> GetAlphabeticalByCategoryWithPaginationAsync<T>(Category category, int pageSize, int pageNo)
+        {
+            if (pageSize > 0)
+            {
+                int offset = (pageNo - 1) * pageSize;
+
+                int totalNoOfEntries = CountEntries<News>();
+
+                if (offset > totalNoOfEntries)
+                {
+                    return (null, 0);
+                }
+
+                IEnumerable<News> returnList;
+
+                if (totalNoOfEntries < offset + pageSize)
+                {
+                    returnList = await Context
+                        .Set<News>()
+                        .Where(n => n.CategoryId == category.Id)
+                        .OrderBy(n => n.Title)
+                        .Skip(offset)
+                        .Take(totalNoOfEntries - offset)
+                        .ToListAsync();
+                }
+                else
+                {
+                    returnList = await Context
+                        .Set<News>()
+                        .Where(n => n.CategoryId == category.Id)
+                        .OrderBy(n => n.Title)
+                        .Skip(offset)
+                        .Take(pageSize)
+                        .ToListAsync();
+                }
+
+                return (returnList, totalNoOfEntries);
+            }
+
+            return (null, 0);
+        }
+
+        public async Task<IEnumerable<News>> GetLatestAsync()
+        {
+            return await Context
+                .Set<News>()
+                .OrderByDescending(n => n.LastEditDate)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<News>> GetLatestByCategoryAsync(Category category)
+        {
+            return await Context
+                .Set<News>()
+                .Where(n => n.CategoryId == category.Id)
+                .OrderByDescending(n => n.LastEditDate)
+                .ToListAsync();
+        }
+
+        public async Task<(IEnumerable<News>, int)> GetLatestByCategoryWithPaginationAsync<T>(Category category, int pageSize, int pageNo)
+        {
+            if (pageSize > 0)
+            {
+                int offset = (pageNo - 1) * pageSize;
+
+                int totalNoOfEntries = CountEntries<News>();
+
+                if (offset > totalNoOfEntries)
+                {
+                    return (null, 0);
+                }
+
+                IEnumerable<News> returnList;
+
+                if (totalNoOfEntries < offset + pageSize)
+                {
+                    returnList = await Context
+                        .Set<News>()
+                        .Where(n => n.CategoryId == category.Id)
+                        .OrderByDescending(n => n.LastEditDate)
+                        .Skip(offset)
+                        .Take(totalNoOfEntries - offset)
+                        .ToListAsync();
+                }
+                else
+                {
+                    returnList = await Context
+                        .Set<News>()
+                        .Where(n => n.CategoryId == category.Id)
+                        .OrderByDescending(n => n.LastEditDate)
+                        .Skip(offset)
+                        .Take(pageSize)
+                        .ToListAsync();
+                }
+
+                return (returnList, totalNoOfEntries);
+            }
+
+            return (null, 0); ;
+        }
+
+        public async Task<(IEnumerable<News>, int)> GetLatestWithPaginationAsync<T>(int pageSize, int pageNo)
+        {
+            if (pageSize > 0)
+            {
+                int offset = (pageNo - 1) * pageSize;
+
+                int totalNoOfEntries = CountEntries<News>();
+
+                if (offset > totalNoOfEntries)
+                {
+                    return (null, 0);
+                }
+
+                IEnumerable<News> returnList;
+
+                if (totalNoOfEntries < offset + pageSize)
+                {
+                    returnList = await Context.Set<News>().OrderByDescending(n => n.LastEditDate).Skip(offset).Take(totalNoOfEntries - offset).ToListAsync();
+                }
+                else
+                {
+                    returnList = await Context.Set<News>().OrderByDescending(n => n.LastEditDate).Skip(offset).Take(pageSize).ToListAsync();
+                }
+
+                return (returnList, totalNoOfEntries);
+            }
+
+            return (null, 0);
+        }
+
+        public async Task<IEnumerable<News>> GetNonAlpabeticalByCategoryAsync(Category category)
+        {
+            return await Context
+                .Set<News>()
+                .Where(n => n.CategoryId == category.Id)
+                .OrderByDescending(n => n.Title)
+                .ToListAsync();
+        }
+
+        public async Task<(IEnumerable<News>, int)> GetNonAlphabeticalByCategoryWithPaginationAsync<T>(Category category, int pageSize, int pageNo)
+        {
+            if (pageSize > 0)
+            {
+                int offset = (pageNo - 1) * pageSize;
+
+                int totalNoOfEntries = CountEntries<News>();
+
+                if (offset > totalNoOfEntries)
+                {
+                    return (null, 0);
+                }
+
+                IEnumerable<News> returnList;
+
+                if (totalNoOfEntries < offset + pageSize)
+                {
+                    returnList = await Context
+                        .Set<News>()
+                        .Where(n => n.CategoryId == category.Id)
+                        .OrderByDescending(n => n.Title)
+                        .Skip(offset)
+                        .Take(totalNoOfEntries - offset)
+                        .ToListAsync();
+                }
+                else
+                {
+                    returnList = await Context
+                        .Set<News>()
+                        .Where(n => n.CategoryId == category.Id)
+                        .OrderByDescending(n => n.Title)
+                        .Skip(offset)
+                        .Take(pageSize)
+                        .ToListAsync();
+                }
+
+                return (returnList, totalNoOfEntries);
+            }
+
+            return (null, 0); ;
+        }
+
+        public async Task<IEnumerable<News>> GetOldestByCategoryAsync(Category category)
+        {
+            return await Context
+                .Set<News>()
+                .Where(n => n.CategoryId == category.Id)
+                .OrderBy(n => n.LastEditDate)
+                .ToListAsync();
+        }
+
+        public async Task<(IEnumerable<News>, int)> GetOldestByCategoryWithPaginationAsync<T>(Category category, int pageSize, int pageNo)
+        {
+            if (pageSize > 0)
+            {
+                int offset = (pageNo - 1) * pageSize;
+
+                int totalNoOfEntries = CountEntries<News>();
+
+                if (offset > totalNoOfEntries)
+                {
+                    return (null, 0);
+                }
+
+                IEnumerable<News> returnList;
+
+                if (totalNoOfEntries < offset + pageSize)
+                {
+                    returnList = await Context
+                        .Set<News>()
+                        .Where(n => n.CategoryId == category.Id)
+                        .OrderBy(n => n.LastEditDate)
+                        .Skip(offset)
+                        .Take(totalNoOfEntries - offset)
+                        .ToListAsync();
+                }
+                else
+                {
+                    returnList = await Context
+                        .Set<News>()
+                        .Where(n => n.CategoryId == category.Id)
+                        .OrderBy(n => n.LastEditDate)
+                        .Skip(offset)
+                        .Take(pageSize)
+                        .ToListAsync();
+                }
+
+                return (returnList, totalNoOfEntries);
+            }
+
+            return (null, 0);
         }
     }
 }
--- NewsIO/NewsIO.Services/Intefaces/INewsService.cs
+++ NewsIO/NewsIO.Services/Intefaces/INewsService.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using NewsIO.Data.Models.Application;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NewsIO.Services.Intefaces
 {
     public interface INewsService : IGenericDbService
     {
+        Task<IEnumerable<News>> GetLatestAsync();
+
+        Task<(IEnumerable<News>, int)> GetLatestWithPaginationAsync<T>(int pageSize, int pageNo);
+
+        Task<IEnumerable<News>> GetLatestByCategoryAsync(Category category);
+
+        Task<(IEnumerable<News>, int)> GetLatestByCategoryWithPaginationAsync<T>(Category category, int pageSize, int pageNo);
+
+        Task<IEnumerable<News>> GetAlpabeticalByCategoryAsync(Category category);
+
+        Task<(IEnumerable<News>, int)> GetAlphabeticalByCategoryWithPaginationAsync<T>(Category category, int pageSize, int pageNo);
+
+        Task<IEnumerable<News>> GetOldestByCategoryAsync(Category category);
+
+        Task<(IEnumerable<News>, int)> GetOldestByCategoryWithPaginationAsync<T>(Category category, int pageSize, int pageNo);
+
+        Task<IEnumerable<News>> GetNonAlpabeticalByCategoryAsync(Category category);
+
+        Task<(IEnumerable<News>, int)> GetNonAlphabeticalByCategoryWithPaginationAsync<T>(Category category, int pageSize, int pageNo);
     }
 }
